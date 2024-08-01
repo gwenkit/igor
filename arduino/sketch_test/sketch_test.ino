e643e91f954d56ace432675467e392cbd5667772
gwenkit.com <build@gwenkit.com>
2024-08-01 21:39:26 +0900

    Hello, world!

--- arduino/sketch_test/sketch_test.ino
+++ arduino/sketch_test/sketch_test.ino
@@ -1,5 +1,29 @@
+// Hello, world!
+
+#define DEBUG 0
+
+String buffer;
+
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, world!");
+
+  print_help();
+  clear_buffer();
 }
 
 void loop() {
+}
+
+void print_help() {
+}
+
+void debug_buffer() {
+  if (DEBUG) {
+    Serial.println(String("[DEBUG] buffer[") + buffer + "]");
+  }
+}
+
+void clear_buffer() {
+  buffer = "";
 }
