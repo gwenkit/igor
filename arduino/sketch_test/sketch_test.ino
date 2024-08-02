93bfdb3aea28c97d89ade7f3cc728641c7ddbfc7
gwenkit.com <build@gwenkit.com>
2024-08-02 16:35:54 +0900

    workspace 테스트

--- arduino/sketch_test/sketch_test.ino
+++ arduino/sketch_test/sketch_test.ino
@@ -1,9 +1,18 @@
-// Hello, world!
+/* Hello, world! */
+
+// https://docs.arduino.cc/learn/built-in-libraries/software-serial/
+#include <SoftwareSerial.h>
+
+// https://arduinojson.org/
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
 
 #define DEBUG 0
 
+// global variables
 String buffer;
 
+// put your setup code here, to run once:
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello, world!");
@@ -12,18 +21,22 @@ void setup() {
   clear_buffer();
 }
 
+// put your main code here, to run repeatedly:
 void loop() {
 }
 
+// print HELP messages
 void print_help() {
 }
 
+// print DEBUG messages
 void debug_buffer() {
   if (DEBUG) {
     Serial.println(String("[DEBUG] buffer[") + buffer + "]");
   }
 }
 
+// clear buffer
 void clear_buffer() {
   buffer = "";
 }
