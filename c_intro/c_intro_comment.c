c6681bd5c179ca8337c1a9fadc9788ca862db170
gwenkit.com <build@gwenkit.com>
2024-10-15 16:33:11 +0900

    c_intro

---
+++ c_intro/c_intro_comment.c
@@ -0,0 +1,20 @@
+/*
+ * title: comment
+ * description: example
+ * filename: c_intro_comment.c
+ * create: 2024-10-15
+ * author: GWEN
+ */
+
+#include <stdio.h>
+
+
+/*
+ * entry point
+ */
+int main(void)
+{
+    printf("Hello, world!\n"); // print string
+    return 0; // exit
+} // end of main
+
