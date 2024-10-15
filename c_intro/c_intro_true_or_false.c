ca469619eb184a9ce10e1840b2b2e4572edbf316
gwenkit.com <build@gwenkit.com>
2024-10-15 17:03:59 +0900

    c_intro_true_or_false

---
+++ c_intro/c_intro_true_or_false.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int n, m;
+    n = 10;
+    m = 100;
+    printf("%d == %d ? %d\n", n, m, (n == m));
+    printf("%d <= %d ? %d\n", n, m, (n <= m));
+
+    return 0;
+}
+
+
